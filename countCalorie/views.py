0f07f725475c5280c9cf483b03a5e184d1e77a8e
shublynx <shubham683075@gmail.com>
2023-06-21 17:05:39 +0530

    Calculating total values and displaying them in table

--- countCalorie/views.py
+++ countCalorie/views.py
@@ -5,9 +5,9 @@ from .models import Food,Consume
 def index(request):
     if request.method == "POST":
         food_consumed = request.POST['food_consumed']
-        consumed = Food.objects.get(name=food_consumed)
+        consume = Food.objects.get(name=food_consumed)
         user = request.user
-        consume = Consume(user=user, food_consumed=consumed)
+        consume = Consume(user=user, food_consumed=consume)
         consume.save()
         foods = Food.objects.all()
 
@@ -17,8 +17,6 @@ def index(request):
     consumed_food = Consume.objects.filter(user=request.user)
 
     return render(request, 'countCalorie/index.html', {'food_items': foods, 'consumed_food': consumed_food})
-
-
 def delete(request):
     pass
 
